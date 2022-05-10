18b8f7eb5fa31eea5642cbfef477bfe0ec811fda
dylee111 <dylee0568@gmail.com>
2022-05-10 22:43:46 +0900

    엔티티 추가 -> Calendar 엔티티 추가

---
+++ src/main/java/com/nightovertime/nightovertimecalc/entity/Calendar.java
@@ -0,0 +1,37 @@
+package com.nightovertime.nightovertimecalc.entity;
+
+import com.nightovertime.nightovertimecalc.entity.baseentity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+public class Calendar extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CALENDAR_ID")
+    private Long id;
+
+    @Column(name = "YEAR")
+    private int year;
+
+    @Column(name = "MONTH")
+    private int month;
+
+    @Column(name = "DAY")
+    private int day;
+
+    @Column(name = "TOTAL_COST")
+    private int totalCost;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+}
